03913468b253c23def331b1ae451c753ac4da7d9
JonathanIMG <jdaza@imglatam.com>
2015-04-17 10:02:38 -0500

    Jonathan 10:02

--- slnPrueba/wPrueba/wErikaPrueba.aspx.vb
+++ slnPrueba/wPrueba/wErikaPrueba.aspx.vb
@@ -3,12 +3,20 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        If Not IsPostBack Then
 
+        End If
 
 
     End Sub
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Label1.Text = "Hola Mundo"
+        Me.Label2.Text = "JOnathan"
+        Me.Label3.Text = "JOnathan 1"
+
+
     End Sub
+
+
 End Class
